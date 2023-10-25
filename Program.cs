1f0792a1333fbbc6ae712842e5fbf1a1fd290a04
raluca_stamate <raluca.stamate@student.usv.ro>
2023-10-25 12:07:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Stamate Raluca-Ioana, grupa 3132a, laboratorul 2
+
+namespace stamate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
